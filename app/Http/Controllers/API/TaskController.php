d4398619f56bb5a8d5c462b595d4699372022d08
Jose Ascurra <joseascurra123@gmail.com>
2023-06-21 18:04:11 -0400

    Agregado de soporte para filtros en el listado

--- app/Http/Controllers/API/TaskController.php
+++ app/Http/Controllers/API/TaskController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateTaskMultipleRequest;
 use App\Http\Resources\TaskResource;
+use Illuminate\Http\Request;
 use App\Models\Task;
 
 class TaskController extends Controller
 {
-    public function index(){
-        $tasks = Task::get();
+    public function index(Request $request){
+        $tasks = Task::query();
+        foreach($request->input('filters',[]) as $filter => $value){
+            $tasks->{$filter}($value);
+        }
         return TaskResource::collection($tasks);
     }
 
